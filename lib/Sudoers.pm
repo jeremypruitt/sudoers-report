083ed9155bf1a4a1c4cfaf18677ee250dc90aabe
Jeremy Pruitt <jpruitt@juniper.net>
2012-11-21 01:49:01 -0800

    Initial commit

---
+++ lib/Sudoers.pm
@@ -0,0 +1,297 @@
+#! /usr/local/bin/perl
+
+package Sudoers;
+
+our $VERSION = 'v0.0.2';
+
+use utf8;
+use strict;
+use warnings;
+use YAML::Tiny;
+use Hash::Merge 'merge';
+use English qw( -no_match_vars );
+use Getopt::Long;
+use Pod::Usage qw( pod2usage );
+
+Hash::Merge::set_behavior( 'RETAINMENT_PRECEDENT' );
+
+sub trim {
+    (my $s = $_[0]) =~ s/^\s+|\s+$//g;
+    return $s;        
+}
+
+sub process_sudo_spec {
+  my ($line,$spec) = @_;
+  $line = trim($line);
+
+  $line =~ /\s*(\S+)\s+\S+\s*=\s*.+/;
+  my $user = $1;
+  foreach my $foo ($line =~ m/\S+\s*=\s*[^:]+[$:]?/g) {
+    $foo =~ /\s*(\S+)\s*=\s*(.+)\s*(?::|$)/;
+    my $host = $1;
+    my $cmnd = $2;
+    $spec->{$user}->{$host}->{'Cmnd_Alias'}->{$cmnd} = '1';
+  }
+
+  return $spec;
+}
+
+sub process_sudo_alias {
+  my ($line,$alias) = @_;
+  $line = trim($line);
+  my ($name,$raw_value) = split(/\s*=\s*/,$line,2);
+  my @values = split(/\s*,\s*/,$raw_value);
+  $alias->{$name} = \@values;
+  return $alias;
+}
+
+sub process_line {
+  my ($line,$sudoers) = @_;
+
+  chomp($line);
+
+  if ($line =~ s/^\s*Host_Alias\s+//) {
+    $sudoers->{'Host_Alias'} = process_sudo_alias($line,$sudoers->{'Host_Alias'});
+  } elsif ($line =~ s/^\s*User_Alias\s+//) {
+    $sudoers->{'User_Alias'} = process_sudo_alias($line,$sudoers->{'User_Alias'});
+  } elsif ($line =~ s/^\s*Cmnd_Alias\s+//) {
+    $sudoers->{'Cmnd_Alias'} = process_sudo_alias($line,$sudoers->{'Cmnd_Alias'});
+  } elsif ($line =~ s/^\s*Runas_Alias\s+//) {
+    $sudoers->{'Runas_Alias'} = process_sudo_alias($line,$sudoers->{'Runas_Alias'});
+  } elsif ($line =~ /^\s*\S+\s+\S+\s*=\s*\S+/) {
+    $sudoers->{'Spec'} = process_sudo_spec($line,$sudoers->{'Spec'});
+  } else {
+    print "LINE: ${line}\n";
+  }
+
+  return $sudoers
+}
+
+sub build_sudoers_hash_from_file {
+  my ($sudoers_file) = @_; 
+
+  open(my $sudoers_fh, '<', $sudoers_file) or die "Could not open '$sudoers_file' $!\n";
+ 
+  my $sudoers;
+
+  while (my $line = <$sudoers_fh>) {
+    next if $line =~ /^\s*(#|$)/;
+
+    if ($line =~ s/\\\s*$//) {
+        $line .= <$sudoers_fh>;
+        redo unless eof($sudoers_fh);
+    }
+
+    $sudoers = process_line($line,$sudoers);
+  }
+
+  return $sudoers;
+}
+
+sub host_alias_report_for {
+  my ($host_alias,$sudoers) = @_;
+
+  my $report;
+  my %spec = %{ $sudoers->{'Spec'} };
+
+  foreach my $user_alias ( keys(%spec) ) {
+    if ($spec{$user_alias}{$host_alias}) {
+      my %cmnd_alias_tuple = %{ $spec{$user_alias}{$host_alias}{'Cmnd_Alias'} };
+      my @cmnd_alias = keys(%cmnd_alias_tuple);
+      $report->{$user_alias} = $cmnd_alias[0];
+    }
+  };
+
+  return $report;
+}
+
+sub process_host_alias{
+  my ($host_alias,$spec_ref) = @_;
+
+  my $report;
+  my %spec = %{ $spec_ref };
+
+  foreach my $user_alias ( keys(%$spec_ref) ) {
+    if ($spec{$user_alias}{$host_alias}) {
+      my %cmnd_alias_tuple = %{ $spec{$user_alias}{$host_alias}{'Cmnd_Alias'} };
+      my @cmnd_alias = keys(%cmnd_alias_tuple);
+      $report->{$user_alias} = $cmnd_alias[0];
+    }
+  }
+  return $report;
+}
+
+sub query_hostname {
+  my ($hostname,$sudoers) = @_;
+
+  my $result;
+  my @results;
+  my %spec = %{ $sudoers->{'Spec'} };
+
+  my $host_aliases = get_host_alias_names_for_hostname($hostname,$sudoers);
+  foreach my $host_alias_name (@$host_aliases) {
+    my $processed_host_alias = process_host_alias($host_alias_name,$sudoers->{'Spec'});
+    if ($processed_host_alias && $result) {
+      $result = merge($processed_host_alias,$result);
+    } elsif ($processed_host_alias) {
+      $result = $processed_host_alias;
+    };
+  }
+
+  return $result;
+}
+
+sub host_report {
+  my ($hostname,$sudoers) = @_;
+
+  my $user_specs = query_hostname($hostname,$sudoers);
+  my $relevant_user_aliases = { };
+  my @user_alias_names = keys(%{$user_specs});
+  foreach my $user_alias_name (@user_alias_names) {
+    my $user_alias = $sudoers->{'User_Alias'}{$user_alias_name};
+    foreach my $username (@{$user_alias}) {
+      if ($username =~ /^[A-Z0-9_]+$/) {
+        my $inner_user_alias = $sudoers->{'User_Alias'}{$username};
+        $relevant_user_aliases->{$username} = $inner_user_alias;
+      }
+    }
+    $relevant_user_aliases->{$user_alias_name} = $user_alias;
+  }
+
+  return {
+    'User Specs'   => $user_specs,
+    'User Aliases' => $relevant_user_aliases,
+  };
+}
+
+sub get_host_alias_names_for_hostname {
+  my ($hostname,$sudoers) = @_;
+  my @found_host_aliases;
+
+  my %host_alias = %{ $sudoers->{'Host_Alias'} };
+
+  foreach my $host_alias_name ( keys(%host_alias) ) {
+    my @host_aliases = @{ $host_alias{$host_alias_name} };
+    if (grep {$_ eq $hostname} @host_aliases) {
+      push(@found_host_aliases, $host_alias_name);
+    }
+  }
+
+  return \@found_host_aliases;
+}
+
+sub main {
+    my ($self) = @_;
+    my ($filename,$hostname);
+
+    my $parser = Getopt::Long::Parser->new();
+    $parser->configure( 'bundling', 'no_ignore_case', );
+
+    my $cli_options = {
+        'H|help|?' => sub { pod2usage( -verbose => 1 ) },
+        'man'      => sub { pod2usage( -verbose => 2 ) },
+        'usage'    => sub { pod2usage( -verbose => 0 ) },
+        'version'  => sub { print "version: $VERSION\n"; exit 1; },
+        'f|filename=s' => \$filename,
+        'h|hostname=s' => \$hostname,
+    };
+
+    $parser->getoptions( %{$cli_options} ) or die "Incorrect usage.\n";
+    if(!defined($hostname)){ pod2usage(-verbose => 0) }
+    if(!defined($filename)){ pod2usage(-verbose => 0) }
+
+    my $sudoers = build_sudoers_hash_from_file($filename);
+    print Dump host_report($hostname,$sudoers);
+    return;
+}
+
+__PACKAGE__->main() unless caller;
+
+1;
+
+__END__
+
+#-----------------------------------------------------------------------------
+
+=pod
+
+=encoding utf8
+
+=head1 NAME
+
+C<sudoers-report> - Generates a report of the authorizations allowed for a
+                    specific serverin a given sudoers file.
+
+=head1 USAGE
+
+sudoers-report --filename /etc/sudoers --hostname example
+
+=head1 REQUIRED ARGUMENTS
+
+=head1 ARGUMENTS
+
+=head1 OPTIONS
+
+These are the application options.
+
+=over
+
+=item B<-?, --help>
+
+Displays a brief summary of options and exits.
+
+=item B<--man>
+
+Displays the complete manual and exits.
+
+=item B<--usage>
+
+Displays the basic application usage.
+
+=item B<--version>
+
+Displays the version number and exits.
+
+=item B<--filename>
+
+Accepts a path to the sudoers file
+
+=item B<--hostname>
+
+Accepts a hostname to report
+
+=back
+
+=head1 DESCRIPTION
+
+This application can do < x, y, and z >.
+
+=head1 DIAGNOSTICS
+
+=head1 EXIT STATUS
+
+0 - Sucessful program execution.
+1 - Program exited normally. --help, --man, and --version return 1.
+2 - Program exited normally. --usage returns 2.
+
+=head1 CONFIGURATION
+
+=head1 DEPENDENCIES
+
+=head1 INCOMPATIBILITIES
+
+=head1 BUGS AND LIMITATIONS
+
+=head1 HOMEPAGE
+
+http://www.github.com/juniper/sudoers-report
+
+=head1 AUTHOR
+
+Name <jpruitt@juniper.net>
+
+=head1 LICENSE AND COPYRIGHT
+
+Copyright (c) 2012 Juniper Networks. All rights reserved.
+
+=cut
---
+++ t/Sudoers.t
@@ -0,0 +1,132 @@
+#!/usr/local/bin/perl
+
+use strict;
+use warnings;
+use Test::More 'no_plan';
+use YAML::Tiny;
+
+use lib '.';
+
+my $class = 'Sudoers';
+
+##
+## Use module and call 'main' subroutine
+##
+use_ok( $class );
+can_ok( $class, 'main' );
+
+##
+## Process a Host_Alias
+##
+my $host_alias_string         = 'SERVERS=box1.example.com,box2.example.com';
+my $expected_host_alias_tuple = { 'SERVERS' => [ 'box1.example.com', 'box2.example.com' ] };
+my $actual_host_alias_tuple   = Sudoers::process_sudo_alias($host_alias_string,{ });
+is_deeply($actual_host_alias_tuple, $expected_host_alias_tuple, 'Processed Host_Alias string');
+
+##
+## Process a Host_Alias Line
+##
+my $host_alias_line       = 'Host_Alias    SERVERS=box1.example.com,box2.example.com';
+my $expected_sudoers_hash = { 'Host_Alias' => { 'SERVERS' => [ 'box1.example.com', 'box2.example.com' ] } };
+my $actual_sudoers_hash   = Sudoers::process_line($host_alias_line,{ });
+is_deeply($actual_sudoers_hash, $expected_sudoers_hash, 'Processed Host_Alias line');
+
+##
+## Process a User_Alias
+##
+my $user_alias_string         = 'ADMINS=juser,toor';
+my $expected_user_alias_tuple = { 'ADMINS' => [ 'juser', 'toor' ] };
+my $actual_user_alias_tuple   = Sudoers::process_sudo_alias($user_alias_string,{ });
+is_deeply($actual_user_alias_tuple, $expected_user_alias_tuple, 'Processed User_Alias string');
+
+##
+## Process a User_Alias Line
+##
+my $user_alias_line    = 'User_Alias    ADMINS=juser,toor';
+my $expected_user_hash = { 'User_Alias' => { 'ADMINS' => [ 'juser', 'toor' ] } };
+my $actual_user_hash   = Sudoers::process_line($user_alias_line,{ });
+is_deeply($actual_user_hash, $expected_user_hash, 'Processed User_Alias line');
+
+##
+## Process a Sudo Spec #1
+##
+# Spec: %test13   WORKSTATIONS=(ALL) ALL
+# Spec: SUSADMINS sustaining-eng=(ALL) ALL
+my $sudo_spec_line1     = 'ADMINS      SERVERS=(ALL) ALL';
+my $expected_sudo_spec1 = {
+  'ADMINS' => {
+    'SERVERS' => {
+      'Cmnd_Alias' => { '(ALL) ALL' => '1' },
+    },
+  },
+};
+my $actual_sudo_spec1 = Sudoers::process_sudo_spec($sudo_spec_line1,{ });
+is_deeply($actual_sudo_spec1, $expected_sudo_spec1, 'Processed Sudo Spec line: User_Alias (ALL) ALL');
+
+##
+## Process a Sudo Spec #2
+##
+# Spec: %test13   WORKSTATIONS=(ALL) ALL
+# Spec: SUSADMINS sustaining-eng=(ALL) ALL
+my $sudo_spec_line2     = '%test   WORKSTATIONS=(ALL) ALL';
+my $expected_sudo_spec2 = {
+  '%test' => {
+    'WORKSTATIONS' => {
+      'Cmnd_Alias' => { '(ALL) ALL' => '1' },
+    },
+  },
+};
+my $actual_sudo_spec2 = Sudoers::process_sudo_spec($sudo_spec_line2,{ });
+is_deeply($actual_sudo_spec2, $expected_sudo_spec2, 'Processed Sudo Spec line: Unix Group (ALL) ALL');
+
+my $sudoers = Sudoers::build_sudoers_hash_from_file('./sudoers.example');
+
+##
+## get_host_alias_names_for_hostname
+##
+my $hostname = 'desktop2';
+my @expected_host_aliases = [
+  'ENGINEERING',
+  'WORKSTATIONS',
+];
+my $actual_host_aliases = Sudoers::get_host_alias_names_for_hostname($hostname,$sudoers);
+is_deeply($actual_host_aliases, @expected_host_aliases, "Got host aliases for $hostname")
+  or diag explain $actual_host_aliases;
+
+
+##
+## process_host_report for host alias
+##
+my $host_alias = 'WORKSTATIONS';
+my $expected_host_alias_result = {
+  '%root' => '(ALL) ALL',
+  '%wheel' => '(ALL) ALL',
+  'ADMINS' => '(ALL) ALL',
+  'BACKUP_ADMINS' => '(ALL) /bin/backup',
+};
+my $actual_host_alias_result = Sudoers::process_host_alias($host_alias,$sudoers->{'Spec'});
+is_deeply($actual_host_alias_result, $expected_host_alias_result, "Got host report for host alias: $host_alias")
+  or diag explain $actual_host_alias_result;
+
+##
+## host_report_for hostname
+##
+$hostname = 'desktop2';
+my $expected_query_hostname_result = {
+  '%root' => '(ALL) ALL',
+  '%wheel' => '(ALL) ALL',
+  'ADMINS' => [
+    '(ALL) ALL',
+    '(ALL) /bin/ls',
+  ],
+  'BACKUP_ADMINS' => [
+    '(ALL) /bin/backup',
+    '(ALL) /bin/backup',
+  ],
+};
+my $actual_query_hostname_result = Sudoers::query_hostname($hostname,$sudoers);
+is_deeply(
+  $actual_query_hostname_result,
+  $expected_query_hostname_result,
+  "Got host report for hostname: $hostname"
+) or diag explain $actual_query_hostname_result;
